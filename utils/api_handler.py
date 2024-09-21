d6540a938a96e4a92eba753ca8119c38929b486f
karthik <karthik94833@gmail.com>
2024-09-21 21:21:56 +0530

    Added MongoDB connection in api_handler.py

--- utils/api_handler.py
+++ utils/api_handler.py
@@ -1,1 +1,6 @@
-ECHO is on.
+from pymongo import MongoClient
+
+def get_db():
+    client = MongoClient("mongodb://localhost:27017")
+    db = client["mongodb"]  # Replace with your database name
+    return db
